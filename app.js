dfcd2e783bc8cc8035961b0484a5abb87a74e256
CORTEZZ <sorikson083@gmail.com>
2024-04-09 12:27:01 +0500

    add gitignore

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var express = require("express");
+var express = require('express');
 
 const app = express();
+const router = express.Router();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.listen(7545, () => {
-  console.log("Сервер запущен... (port: 7545)");
+app.use('/', function (_, response) {
+  response.redirect('/pages/main.html');
 });
+
+app.listen(5000, () => console.log('Сервер запущен... (port: 5000)'));
