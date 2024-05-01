b80383477a89ee2ad864e2e480963d48220e573f
CORTEZZ <sorikson083@gmail.com>
2024-05-01 21:39:53 +0500

    add mongo-driver / dotenv & drop nodejs code

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,3 +7,11 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Тест"))
 }
+
+func register(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Тест"))
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Тест"))
+}
