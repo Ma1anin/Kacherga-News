719d9f26422dc42c204003875ec545512c30f7ba
CORTEZZ <116988389+cortezzIP@users.noreply.github.com>
2024-05-03 09:35:04 +0500

    Update test.ts

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -4,12 +4,13 @@ import { app } from "./app";
 it("should return correct user", function(done){
     request(app)
         .get("/user/1")
-        .expect({
-            login: "cortezz",
-            password: "null",
-            name: "Rodrigo Milk",
-            role: "super-admin",
-            picture: "null.jpg"
+        .expect(function(response){
+            assert.deepStrictEqual(response.body, {
+                login: "cortezz",
+                name: "Rodrigo Milk",
+                role: "super-admin",
+                picture: "null.jpg"
+            });
         })
         .end(done);
 });
