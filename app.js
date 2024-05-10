2a8120d0d981cb67cd1b847cb83f08bb65e47dd1
CORTEZZ <sorikson083@gmail.com>
2024-05-10 20:45:17 +0500

    drop front kakish

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const fileUpload = require("express-fileupload");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
-const { validationResult } = require("express-validator");
 
 const newsRouter = require("./src/routes/news.routes");
 const eventRouter = require("./src/routes/event.routes");
@@ -14,12 +13,8 @@ const userRouter = require("./src/routes/user.routes");
 
 const app = express();
 
-const urlencodedParser = bodyParser.urlencoded({
-  extended: false,
-});
-
-app.use(express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(
   session({
@@ -38,75 +33,6 @@ app.use(
 app.use("/api", newsRouter);
 app.use("/api", eventRouter);
 app.use("/api", userRouter);
-
-app.set("view engine", "hbs");
-app.set("views", "public/views");
-
-app.get("/edit-account", function (req, res) {
-  res.render("edit-account.hbs");
-});
-
-app.get("/register", function (req, res) {
-  res.render("register.hbs");
-});
-
-app.get("/login", function (req, res) {
-  res.render("login.hbs");
-});
-
-app.get("/", function (req, res) {
-  res.render("main.hbs");
-});
-
-app.post("/register", urlencodedParser, async function (req, res) {
-  try {
-    if (!req.body) throw new Error("Request body is empty!");
-    await fetch("http://localhost:3000/api/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(req.body),
-    })
-      .then((response) => {
-        if (!response.ok) throw new Error("Create user request was not ok");
-        res.status(200).redirect("/login");
-        // ИСПРАВИТЬ ЭТОТ КОСТЫЛЬ!
-        return;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.post("/login", urlencodedParser, async function (req, res) {
-  try {
-    const result = validationResult(req.body);
-    if (!result.isEmpty()) throw new Error("Request body is empty!");
-    console.log(req.body);
-    await fetch("http://localhost:3000/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(req.body),
-    })
-      .then((response) => {
-        if (!response.ok) throw new Error("Login user request was not ok");
-        res.status(200).redirect("/");
-        // ИСПРАВИТЬ ЭТОТ КОСТЫЛЬ!
-        return;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  } catch (err) {
-    res.status(401).json({message: err.message});
-  }
-});
 
 async function startApp() {
   try {
